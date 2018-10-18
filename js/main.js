ba2911a624c4dddbd7e3ef30241dcf0838e788f0
giganticimplode <sergejcolovic95@gmail.com>
2018-10-18 20:03:11 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Select DOM Items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItem = document.querySelectorAll(".nav-item");
+
+// Set Iniial State Of Menu
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItem.forEach(item => item.classList.add("show"));
+
+    // Set Menu State
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("show");
+    menu.classList.remove("close");
+    menuNav.classList.remove("close");
+    menuBranding.classList.remove("close");
+    navItem.forEach(item => item.classList.remove("show"));
+
+    // Set Menu State
+
+    showMenu = false;
+  }
+}
